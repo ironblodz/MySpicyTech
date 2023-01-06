df747bdd1a410e16e34b2e392332d72aa088eaf8
prog1-2022 <f.sjoaninho23@gmail.com>
2023-01-06 01:06:35 +0000

    update design index.blade.php | add icons and change color

--- resources/views/frontoffice/pages/index.blade.php
+++ resources/views/frontoffice/pages/index.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
 <div class="body">
-    <div role="main" class="main">
 
-        <section class="section custom-circles-container section-angled bg-dark border-0 m-0" style="background-image: url(img/demos/digital-agency-2/bg/bg-1.jpg); background-size: cover; background-position: center;">
+    <div role="main" class="main">
+        <!-- ==== Header Principal ==== -->
+        <section class="section custom-circles-container section-angled bg-dark border-0 m-0" style=" background-size: cover; background-position: center;">
             <span class="custom-circle custom-circle-1 bg-color-light custom-circle-blur appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="100" data-plugin-options="{'forceAnimation': true}"></span>
             <span class="custom-circle custom-circle-2 bg-color-primary appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="200" data-plugin-options="{'forceAnimation': true}"></span>
             <span class="custom-circle custom-circle-3 bg-color-primary appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="300" data-plugin-options="{'forceAnimation': true}"></span>
@@ -17,9 +18,9 @@
                 <div class="container pt-md-5 pb-lg-4 mt-5 mb-lg-5">
                     <div class="row pb-lg-5 mt-5 mb-lg-5">
                         <div class="col-lg-7 pt-5 pt-md-0 pb-lg-5 mt-5 mb-md-5">
-                            <h2 class="custom-title-with-icon custom-title-with-icon-primary font-weight-normal text-color-default text-5 mb-0 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="600">Welcome to the SpicyTecH</h2>
-                            <h1 class="text-color-light font-weight-extra-bold text-10 text-md-12-13 line-height-2 mb-4 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="850">Turn your ideas into reality SpicyTecH.</h1>
-                            <a href="#intro" data-hash data-hash-offset="0" data-hash-offset-lg="100" class="btn btn-outline custom-btn-outline btn-primary rounded-0 font-weight-semibold text-color-light bg-color-hover-primary custom-btn-with-arrow text-4 btn-px-4 py-3 mt-2 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="1100">Get Started</a>
+                            <h2 class="custom-title-with-icon custom-title-with-icon-primary font-weight-normal text-color-default text-5 mb-0 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="600">Bem vindos à SpicyTecH</h2>
+                            <h1 class="text-color-light font-weight-extra-bold text-10 text-md-12-13 line-height-2 mb-4 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="850">Transforme ideias em Websites  -> <span class="text-style">S</span>picy<span class="text-style">T</span>ecH.</h1>
+                            <a href="#intro" data-hash data-hash-offset="0" data-hash-offset-lg="100" class="btn btn-outline custom-btn-outline btn-primary rounded-0 font-weight-semibold text-color-light bg-color-hover-primary custom-btn-with-arrow text-4 btn-px-4 py-3 mt-2 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="1100">Contacte-nos</a>
                         </div>
                     </div>
                     <div class="spacer py-5 mt-lg-5 mb-md-5"></div>
@@ -27,32 +28,36 @@
             </div>
         </section>
 
+        <!-- ===== CARDS INFO ===== -->
         <section class="custom-cards p-relative mb-5 pb-5 z-index-2">
             <div class="container">
                 <div class="row">
+                    <!-- ==== card info 1 ==== -->
                     <div class="col-lg-4 col-custom-cards">
                         <div class="card border-0 bg-color-dark rounded-0 z-index-1 p-5 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="100">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center p-0 my-3">
-                                <img src="img/demos/digital-agency-2/icons/solution.png" alt="Solution" class="mb-5">
-                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Growth Marketing</h4>
+                                <img src="/assets/img/demos/digital-agency-2/icons/responsive.png" alt="Solution" class="mb-5"  style=" max-width: 80px; background-size: cover;">
+                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Responsivo</h4>
                                 <p class="card-text text-center custom-text-4 font-weight-lighter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra erat orci, ac auctor</p>
                             </div>
                         </div>
                     </div>
+                    <!-- ==== card info 2 ==== -->
                     <div class="col-lg-4 col-custom-cards">
                         <div class="card border-0 bg-color-primary rounded-0 z-index-1 p-5 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center p-0 my-3">
-                                <img src="img/demos/digital-agency-2/icons/advertising.png" alt="Solution" class="mb-5">
-                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Content Studio</h4>
+                                <img src="/assets/img/demos/digital-agency-2/icons/shield.png" alt="Solution" class="mb-5" style=" max-width: 100px; "">
+                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Seguro</h4>
                                 <p class="card-text text-center custom-text-4 font-weight-lighter text-color-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra erat orci, ac auctor</p>
                             </div>
                         </div>
                     </div>
+                    <!-- ==== card info 3 ==== -->
                     <div class="col-lg-4 col-custom-cards">
                         <div class="card border-0 bg-color-dark rounded-0 z-index-1 p-5 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="300">
                             <div class="card-body d-flex flex-column justify-content-center align-items-center p-0 my-3">
-                                <img src="img/demos/digital-agency-2/icons/feedback.png" alt="Solution" class="mb-5">
-                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Digital Marketing</h4>
+                                <img src="/assets/img/demos/digital-agency-2/icons/stars.png" alt="Solution" class="mb-5" style=" max-width: 110px; background-size: cover;">
+                                <h4 class="card-title custom-text-8 font-weight-bold text-color-light text-center mb-3">Elegante</h4>
                                 <p class="card-text text-center custom-text-4 font-weight-lighter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra erat orci, ac auctor</p>
                             </div>
                         </div>
@@ -61,14 +66,15 @@
             </div>
         </section>
 
+        <!-- ==== Nossa Abordagem ==== -->
         <section class="our-approach py-5 mb-4 mb-xl-5 mt-4 mt-xl-0 p-relative z-index-1" id="intro">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 col-xl-4 d-flex flex-column justify-content-center align-items-start">
-                        <h4 class="custom-text-10 mb-4 pb-2 font-weight-bold custom-title-with-icon custom-title-with-icon-primary appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="100">Our Approach</h4>
+                        <h4 class="custom-text-10 mb-4 pb-2 font-weight-bold custom-title-with-icon custom-title-with-icon-primary appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="100">Nossa abordagem</h4>
                         <p class="custom-font-tertiary custom-text-6 line-height-6 font-weight-medium appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ut tellus ante. Nam suscipit urna risus.</p>
                         <p class="custom-text-3 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet libero id nisi euismod, sed porta est consectetur.</p>
-                        <a herf="demo-digital-agency-2-about-us.html" class="btn btn-outline custom-btn-outline btn-light border-0 rounded-0 text-color-primary custom-text-5 font-weight-bold custom-btn-with-arrow bg-transparent p-0 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="600">Find Out More</a>
+                        <a herf="demo-digital-agency-2-about-us.html" class="btn btn-outline custom-btn-outline btn-light border-0 rounded-0 text-color-primary custom-text-5 font-weight-bold custom-btn-with-arrow bg-transparent p-0 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="600">Descubra mais</a>
                     </div>
                     <div class="col-lg-4 py-4 py-lg-0">
                         <div class="approach-img bg-color-dark">
@@ -103,7 +109,7 @@
                                             <div class="text-start p-relative z-index-2 mb-3">
                                                 <h4 class="text-color-light custom-text-9 font-weight-bold text-decoration-none mb-2">Okler Campaing</h4>
                                                 <p class="text-uppercase custom-text-4 text-color-quaternary text-decoration-none mb-3">search engine optimize, social media ads</p>
-                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">View Work</span>
+                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">Ver projeto</span>
                                             </div>
                                         </div>
                                     </a>
@@ -117,7 +123,7 @@
                                             <div class="text-start p-relative z-index-2 mb-3">
                                                 <h4 class="text-color-light custom-text-9 font-weight-bold text-decoration-none mb-2">Porto Dashboard</h4>
                                                 <p class="text-uppercase custom-text-4 text-color-quaternary text-decoration-none mb-3">content marketing, email marketing, search engine optimize</p>
-                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">View Work</span>
+                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">Ver projeto</span>
                                             </div>
                                         </div>
                                     </a>
@@ -131,7 +137,7 @@
                                             <div class="text-start p-relative z-index-2 mb-3">
                                                 <h4 class="text-color-light custom-text-9 font-weight-bold text-decoration-none mb-2">Okler Responsive Photos</h4>
                                                 <p class="text-uppercase custom-text-4 text-color-quaternary text-decoration-none mb-3">ui design, web development, b2b software</p>
-                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">View Work</span>
+                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">Ver projeto</span>
                                             </div>
                                         </div>
                                     </a>
@@ -145,7 +151,7 @@
                                             <div class="text-start p-relative z-index-2 mb-3">
                                                 <h4 class="text-color-light custom-text-9 font-weight-bold text-decoration-none mb-2">Okler Mobile App</h4>
                                                 <p class="text-uppercase custom-text-4 text-color-quaternary text-decoration-none mb-3">brand and identity, content strategy</p>
-                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">View Work</span>
+                                                <span class="custom-text-4 font-weight-semibold m-0 p-0 text-color-light custom-btn-with-arrow custom-btn-with-arrow-primary text-decoration-none">Ver projeto</span>
                                             </div>
                                         </div>
                                     </a>
@@ -157,7 +163,7 @@
                         <div id="sidebar" class="side-menu-our-work sidebar position-relative">
                             <div data-plugin-sticky data-plugin-options="{'minWidth': 991, 'containerSelector': '.sticky-container', 'padding': {'top': 100}}">
                                 <div class="py-5 my-2 ps-5 pe-0">
-                                    <h4 class="text-color-light custom-text-10 font-weight-bold custom-title-with-icon custom-title-with-icon-primary">Our Work</h4>
+                                    <h4 class="text-color-light custom-text-10 font-weight-bold custom-title-with-icon custom-title-with-icon-primary">Portfólio</h4>
                                     <ul class="list-unstyled sort-source" data-sort-id="portfolio" data-option-key="filter">
                                         <li class="nav-item active" data-option-value="*">
                                             <a href="#ourWork" data-hash data-hash-offset="0" data-hash-offset-lg="100" class="text-color-quaternary text-color-hover-light text-decoration-none mb-2 py-0 d-block">View All</a>
@@ -202,7 +208,7 @@
             <span class="custom-circle custom-circle-2 bg-color-quaternary appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="100"></span>
             <div class="container">
                 <div class="row justify-content-center">
-                    <h4 class="text-color-dark custom-text-10 font-weight-bold text-center custom-title-with-icon-center custom-title-with-icon custom-title-with-icon-primary pb-5 mb-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">We Can Help You With:</h4>
+                    <h4 class="text-color-dark custom-text-10 font-weight-bold text-center custom-title-with-icon-center custom-title-with-icon custom-title-with-icon-primary pb-5 mb-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">Nós ajudamos com:</h4>
                 </div>
                 <div class="row">
                     <div class="col-xl-4 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="200">
@@ -222,7 +228,7 @@
                     <div class="col-xl-4 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="400">
                         <a href="demo-digital-agency-2-our-services-detail.html" class="text-decoration-none">
                             <div class="service-card mb-4 bg-color-tertiary bg-color-hover-primary text-color-hover-light w-100 font-weight-semibold custom-text-6 line-height-6 text-color-dark p-relative">
-                                B2B Software
+                                Marca e Entidade
                             </div>
                         </a>
                     </div>
@@ -255,20 +261,6 @@
                         <a href="demo-digital-agency-2-our-services-detail.html" class="text-decoration-none">
                             <div class="service-card mb-4 bg-color-tertiary bg-color-hover-primary text-color-hover-light w-100 font-weight-semibold custom-text-6 line-height-6 text-color-dark p-relative">
                                 Social Media Ads
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-xl-4 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="900">
-                        <a href="demo-digital-agency-2-our-services-detail.html" class="text-decoration-none">
-                            <div class="service-card mb-4 bg-color-dark bg-color-hover-primary w-100 font-weight-semibold custom-text-6 line-height-6 text-color-light p-relative">
-                                Brand and Identity
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-xl-4 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1000">
-                        <a href="demo-digital-agency-2-our-services-detail.html" class="text-decoration-none">
-                            <div class="service-card mb-4 bg-color-tertiary bg-color-hover-primary text-color-hover-light w-100 font-weight-semibold custom-text-6 line-height-6 text-color-dark p-relative">
-                                Content Strategy
                             </div>
                         </a>
                     </div>
