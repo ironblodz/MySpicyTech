b9df9a1d0b1d61156608f4c36f6b001f3d66b235
prog1-2022 <f.sjoaninho23@gmail.com>
2023-01-11 01:55:12 +0000

    update

--- resources/views/frontoffice/pages/about.blade.php
+++ resources/views/frontoffice/pages/about.blade.php
@@ -6,7 +6,7 @@
 
         <section
             class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7"
-            style=" background-image: url(/assets/img/demos/digital-agency-2/about-us/about.jpeg); background-size: cover; background-position: center; opacity:40%;">
+            style=" background-image: url(/assets/img/demos/digital-agency-2/about-us/about.jpeg); background-size: cover; background-position: center;">
             <div class="container">
                 <div class="row mt-5">
                     <div class="col-md-12 align-self-center p-static order-2 text-center">
